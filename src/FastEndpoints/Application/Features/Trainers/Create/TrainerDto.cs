9f843a6d6d49dd8e23a07042fa08ae900dffc433
Maxime Poulain <maxime.poulain@outlook.be>
2025-10-25 21:51:13 +0200

    remove fast endpoint

--- src/FastEndpoints/Application/Features/Trainers/Create/TrainerDto.cs
+++
@@ -1,9 +0,0 @@
-namespace BLRefactoring.FastEndpoints.Application.Features.Trainers.Create;
-
-public class TrainerDto
-{
-    public Guid Id { get; init; }
-    public string Firstname { get; init; } = null!;
-    public string Lastname { get; init; } = null!;
-    public string Email { get; init; } = null!;
-}
